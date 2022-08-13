9af2a03619a4af1d50e49ca53b82e5031618d66d
Nathan <nathanrjulian@gmail.com>
2022-08-12 19:29:03 -0700

    Basic Movement Functional

--- Orthographic shooter/Assets/PlayerMovement.cs
+++ Orthographic shooter/Assets/PlayerMovement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public float moveForce = 100f;
+    public float drag = 5f;
+    public float decelForce = 50f;
+
+    public Rigidbody PlayerRB;
+
+    Vector3 movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //PlayerRB.MovePosition(PlayerRB.position + movement * moveForce * Time.fixedDeltaTime);
+        PlayerRB.AddForce(movement * moveForce * Time.fixedDeltaTime);
+        PlayerRB.drag = drag;
+        if(movement.x == 0 && movement.z == 0)
+        {
+            Debug.Log("no input, applying decel Force");
+            PlayerRB.AddForce(-PlayerRB.velocity * decelForce * Time.fixedDeltaTime);
+        }
+        Debug.Log(movement);
     }
 }
